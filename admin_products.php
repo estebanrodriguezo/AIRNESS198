059a1551b04e64b5e228d0998813dbe3bbaffb1f
estebanrodriguezo <esteban.rodriguezo@udea.edu.co>
2022-10-12 17:17:28 -0500

    Inicio del CRUD (Añadir y eliminar productos)

---
+++ admin_products.php
@@ -0,0 +1,131 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+   header('location:login.php');
+};
+
+if(isset($_POST['add_product'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $price = mysqli_real_escape_string($conn, $_POST['price']);
+   $details = mysqli_real_escape_string($conn, $_POST['details']);
+   $image = $_FILES['image']['name'];
+   $image_size = $_FILES['image']['size'];
+   $image_tmp_name = $_FILES['image']['tmp_name'];
+   $image_folter = 'imagenes_subidas/'.$image;
+
+   $select_product_name = mysqli_query($conn, "SELECT name FROM `products` WHERE name = '$name'") or die('query failed');
+
+   if(mysqli_num_rows($select_product_name) > 0){
+      $message[] = 'El nombre de este producto ya existe!';
+   }else{
+      $insert_product = mysqli_query($conn, "INSERT INTO `products`(name, details, price, image) VALUES('$name', '$details', '$price', '$image')") or die('query failed');
+
+      if($insert_product){
+         if($image_size > 2000000){
+            $message[] = 'Esta imagen es muy grande!';
+         }else{
+            move_uploaded_file($image_tmp_name, $image_folter);
+            $message[] = 'Producto añadido!';
+         }
+      }
+   }
+
+}
+
+if(isset($_GET['delete'])){
+
+   $delete_id = $_GET['delete'];
+   $select_delete_image = mysqli_query($conn, "SELECT image FROM `products` WHERE id = '$delete_id'") or die('query failed');
+   $fetch_delete_image = mysqli_fetch_assoc($select_delete_image);
+   unlink('uploaded_img/'.$fetch_delete_image['image']);
+   mysqli_query($conn, "DELETE FROM `products` WHERE id = '$delete_id'") or die('query failed');
+   mysqli_query($conn, "DELETE FROM `wishlist` WHERE pid = '$delete_id'") or die('query failed');
+   mysqli_query($conn, "DELETE FROM `cart` WHERE pid = '$delete_id'") or die('query failed');
+   header('location:admin_products.php');
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Productos</title>
+   <link rel="icon" type="image/jpg" href="./Imagenes/airness.jpg" />
+    <!-- font awesome cdn link uso de iconos -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+    <!-- estilo de css   -->
+    <link rel="stylesheet" href="./css/admin_styleAir.css">
+
+</head>
+<body>
+   
+<?php @include 'admin_header.php'; ?>
+
+<section class="add-products">
+
+   <form action="" method="POST" enctype="multipart/form-data">
+      <h3>Añadir nuevo producto</h3>
+      <input type="text" class="box" required placeholder="Ingrese nombre del producto" name="name">
+      <input type="number" min="0" class="box" required placeholder="Ingrese el precio del prducto" name="price">
+      <textarea name="details" class="box" required placeholder="Ingrese descripcion del producto" cols="30" rows="10"></textarea>
+      <input type="file" accept="image/jpg, image/jpeg, image/png" required class="box" name="image">
+      <input type="submit" value="Añadir producto" name="add_product" class="btn">
+   </form>
+
+</section>
+
+<section class="show-products">
+
+   <div class="box-container">
+
+      <?php
+         $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
+         if(mysqli_num_rows($select_products) > 0){
+            while($fetch_products = mysqli_fetch_assoc($select_products)){
+      ?>
+      <div class="box">
+         <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
+         <img class="image" src="imagenes_subidas/<?php echo $fetch_products['image']; ?>" alt="">
+         <div class="name"><?php echo $fetch_products['name']; ?></div>
+         <div class="details"><?php echo $fetch_products['details']; ?></div>
+         <a href="admin_update_product.php?update=<?php echo $fetch_products['id']; ?>" class="option-btn">Actualizar</a>
+         <a href="admin_products.php?delete=<?php echo $fetch_products['id']; ?>" class="delete-btn" onclick="return confirm('eliminar este producto?');">Eliminar</a>
+      </div>
+      <?php
+         }
+      }else{
+         echo '<p class="empty">no hay productos añadidos aun!</p>';
+      }
+      ?>
+   </div>
+   
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="js/admin_script.js"></script>
+
+</body>
+</html>
