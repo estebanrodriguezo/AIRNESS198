c24987cd221bfbcbf1e6fdf3da62c4302c740994
estebanrodriguezo <esteban.rodriguezo@udea.edu.co>
2022-10-12 11:26:13 -0500

    Finalizacion de las paginas del admin

--- admin_page.php
+++ admin_page.php
@@ -24,16 +24,100 @@ if(!isset($admin_id)){
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
     <!-- estilo de css   -->
-    <link rel="stylesheet" href="./css/admin_style.css">
+    <link rel="stylesheet" href="./css/admin_styleAir.css">
 </head>
 <body>
 
 <?php @include 'admin_header.php'; ?>
 
 
+<section class="dashboard">
 
+   <h1 class="title">dashboard</h1>
 
+   <div class="box-container">
 
+      <div class="box">
+         <?php
+            $total_pendings = 0;
+            $select_pendings = mysqli_query($conn, "SELECT * FROM `orders` WHERE payment_status = 'pending'") or die('query failed');
+            while($fetch_pendings = mysqli_fetch_assoc($select_pendings)){
+               $total_pendings += $fetch_pendings['total_price'];
+            };
+         ?>
+         <h3>$<?php echo $total_pendings; ?>/-</h3>
+         <p>Pendientes totales</p>
+      </div>
+
+      <div class="box">
+         <?php
+            $total_completes = 0;
+            $select_completes = mysqli_query($conn, "SELECT * FROM `orders` WHERE payment_status = 'completed'") or die('query failed');
+            while($fetch_completes = mysqli_fetch_assoc($select_completes)){
+               $total_completes += $fetch_completes['total_price'];
+            };
+         ?>
+         <h3>$<?php echo $total_completes; ?>/-</h3>
+         <p>Pagos completados</p>
+      </div>
+
+      <div class="box">
+         <?php
+            $select_orders = mysqli_query($conn, "SELECT * FROM `orders`") or die('query failed');
+            $number_of_orders = mysqli_num_rows($select_orders);
+         ?>
+         <h3><?php echo $number_of_orders; ?></h3>
+         <p>Pedidos realizados</p>
+      </div>
+
+      <div class="box">
+         <?php
+            $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
+            $number_of_products = mysqli_num_rows($select_products);
+         ?>
+         <h3><?php echo $number_of_products; ?></h3>
+         <p>Productos agregados</p>
+      </div>
+
+      <div class="box">
+         <?php
+            $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE user_type = 'user'") or die('query failed');
+            $number_of_users = mysqli_num_rows($select_users);
+         ?>
+         <h3><?php echo $number_of_users; ?></h3>
+         <p>Usuarios normales</p>
+      </div>
+
+      <div class="box">
+         <?php
+            $select_admin = mysqli_query($conn, "SELECT * FROM `users` WHERE user_type = 'admin'") or die('query failed');
+            $number_of_admin = mysqli_num_rows($select_admin);
+         ?>
+         <h3><?php echo $number_of_admin; ?></h3>
+         <p>Administradores</p>
+      </div>
+
+      <div class="box">
+         <?php
+            $select_account = mysqli_query($conn, "SELECT * FROM `users`") or die('query failed');
+            $number_of_account = mysqli_num_rows($select_account);
+         ?>
+         <h3><?php echo $number_of_account; ?></h3>
+         <p>Numero total de cuentas</p>
+      </div>
+
+      <div class="box">
+         <?php
+            $select_messages = mysqli_query($conn, "SELECT * FROM `message`") or die('query failed');
+            $number_of_messages = mysqli_num_rows($select_messages);
+         ?>
+         <h3><?php echo $number_of_messages; ?></h3>
+         <p>Mensajes nuevos</p>
+      </div>
+
+   </div>
+
+</section>
 
 
 
