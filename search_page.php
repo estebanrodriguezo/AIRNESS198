0198d8babb5637431340721b709b74ccd0d6e223
estebanrodriguezo <esteban.rodriguezo@udea.edu.co>
2022-10-20 11:28:21 -0500

    se termino pagina de busqueda

--- search_page.php
+++ search_page.php
@@ -9,6 +9,53 @@ $user_id = $_SESSION['user_id'];
 if(!isset($user_id)){
    header('location:login.php');
 };
+if(isset($_POST['add_to_wishlist'])){
+
+    $product_id = $_POST['product_id'];
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+    
+    $check_wishlist_numbers = mysqli_query($conn, "SELECT * FROM `wishlist` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+    if(mysqli_num_rows($check_wishlist_numbers) > 0){
+        $message[] = 'Ya se ha añadido a la lista de deseados';
+    }elseif(mysqli_num_rows($check_cart_numbers) > 0){
+        $message[] = 'Ya se ha añadido al carrito';
+    }else{
+        mysqli_query($conn, "INSERT INTO `wishlist`(user_id, pid, name, price, image) VALUES('$user_id', '$product_id', '$product_name', '$product_price', '$product_image')") or die('query failed');
+        $message[] = 'producto añadido a la lista de deseados';
+    }
+
+}
+
+if(isset($_POST['add_to_cart'])){
+
+    $product_id = $_POST['product_id'];
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+    $product_quantity = $_POST['product_quantity'];
+
+    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+    if(mysqli_num_rows($check_cart_numbers) > 0){
+        $message[] = 'Ya se ha añadido al carrito';
+    }else{
+
+        $check_wishlist_numbers = mysqli_query($conn, "SELECT * FROM `wishlist` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+        if(mysqli_num_rows($check_wishlist_numbers) > 0){
+            mysqli_query($conn, "DELETE FROM `wishlist` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+        }
+
+        mysqli_query($conn, "INSERT INTO `cart`(user_id, pid, name, price, quantity, image) VALUES('$user_id', '$product_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
+        $message[] = 'producto añadido al carrito';
+    }
+
+}
 
 ?>
 <!DOCTYPE html>
@@ -43,6 +90,46 @@ if(!isset($user_id)){
         <input type="submit" class="btn" value="Buscar" name="search_btn">
     </form>
 </section>
+
+<section class="products" style="padding-top: 0;">
+
+
+   <div class="box-container" >
+
+      <?php
+      if(isset($_POST['search_btn'])){
+         $search_box = mysqli_real_escape_string($conn, $_POST['search_box']);
+         $select_products = mysqli_query($conn, "SELECT * FROM `products` WHERE name LIKE '%{$search_box}%'") or die('query failed');
+         if(mysqli_num_rows($select_products) > 0){
+            while($fetch_products = mysqli_fetch_assoc($select_products)){
+      ?>
+      <form action="" method="POST" class="box">
+         <a href="view_page.php?pid=<?php echo $fetch_products['id']; ?>" class="fas fa-eye"></a>
+         <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
+         <img src="imagenes_subidas/<?php echo $fetch_products['image']; ?>" alt="" class="image">
+         <div class="name"><?php echo $fetch_products['name']; ?></div>
+         <input type="number" name="product_quantity" value="1" min="0" class="qty">
+         <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
+         <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+         <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
+         <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+         <input type="submit" value="añadir a lista de deseos" name="add_to_wishlist" class="option-btn">
+         <input type="submit" value="añadir al carrito" name="add_to_cart" class="btn">
+      </form>
+      <?php
+         }
+            }else{
+                echo '<p class="empty">No se encontro ningun resultado!</p>';
+            }
+        }else{
+            echo '<p class="empty">Busca cualquier cosa!</p>';
+        }
+      ?>
+
+   </div>
+
+</section>
+
 
 <?php @include 'footer.php'; ?>
 
