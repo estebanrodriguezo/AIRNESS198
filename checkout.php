40b0b161fb1c885989f1aba556ded8408dfd14fe
estebanrodriguezo <esteban.rodriguezo@udea.edu.co>
2022-10-24 14:06:37 -0500

    final del check out

--- checkout.php
+++ checkout.php
@@ -11,6 +11,43 @@ if(!isset($user_id)){
 };
 
 
+if(isset($_POST['order'])){
+
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $number = mysqli_real_escape_string($conn, $_POST['number']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $method = mysqli_real_escape_string($conn, $_POST['method']);
+    $address = mysqli_real_escape_string($conn, 'flat no. '. $_POST['flat'].', '. $_POST['street'].', '. $_POST['city'].', '. $_POST['country'].' - '. $_POST['pin_code']);
+    $placed_on = date('d-M-Y');
+
+    $cart_total = 0;
+    $cart_products[] = '';
+
+    $cart_query = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+    if(mysqli_num_rows($cart_query) > 0){
+        while($cart_item = mysqli_fetch_assoc($cart_query)){
+            $cart_products[] = $cart_item['name'].' ('.$cart_item['quantity'].') ';
+            $sub_total = ($cart_item['price'] * $cart_item['quantity']);
+            $cart_total += $sub_total;
+        }
+    }
+
+    $total_products = implode(', ',$cart_products);
+
+    $order_query = mysqli_query($conn, "SELECT * FROM `orders` WHERE name = '$name' AND number = '$number' AND email = '$email' AND method = '$method' AND address = '$address' AND total_products = '$total_products' AND total_price = '$cart_total'") or die('query failed');
+
+    if($cart_total == 0){
+        $message[] = 'El carrito esta vacio!';
+    }elseif(mysqli_num_rows($order_query) > 0){
+        $message[] = 'Tu pedidio ya fue realizado!';
+    }else{
+        mysqli_query($conn, "INSERT INTO `orders`(user_id, name, number, email, method, address, total_products, total_price, placed_on) VALUES('$user_id', '$name', '$number', '$email', '$method', '$address', '$total_products', '$cart_total', '$placed_on')") or die('query failed');
+        mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+        $message[] = 'Tu orden fue recibida exitosamente!';
+    }
+}
+
+
 ?>
 
 <!DOCTYPE html>
